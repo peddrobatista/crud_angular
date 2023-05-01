42dc14625453541fd1aa33dbd7c8bc717de7df2a
pedro-batista <peddrobatista2022@gmail.com>
2023-05-01 14:23:38 -0300

    Atualizando e criando novos componentes...

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MatSortModule } from '@angular/material/sort';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from  '@angular/common';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 registerLocaleData(localePt);
@@ -44,6 +45,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -4,11 +4,12 @@ import { ProductService } from '../product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
+
   product: Product;
 
   constructor(
@@ -16,17 +17,17 @@ export class ProductUpdateComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute
   ) {}
-
+  
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get("id");
-    this.productService.readById(id).subscribe(product => {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe((product) => {
       this.product = product;
     });
   }
 
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage("Produto atualizado com sucesso!");
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage("Produto excluido com sucesso!");
       this.router.navigate(["/products"]);
     });
   }
