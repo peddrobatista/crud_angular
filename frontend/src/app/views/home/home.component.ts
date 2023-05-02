22914207a1dca13b01568fcc8c0cb933f3028c41
pedro-batista <peddrobatista2022@gmail.com>
2023-05-02 14:21:23 -0300

    Atualizando componentes...

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/components/templates/header/header.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  constructor(private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      routeUrl: ''
+    }
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { HeaderService } from 'src/app/components/templates/header/header.service';
 
 @Component({
   selector: 'app-product-crud',
@@ -8,9 +9,14 @@ import {Router} from '@angular/router';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) {
-
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
   }
+  
   ngOnInit(): void {
     
   }
